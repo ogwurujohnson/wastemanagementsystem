f91f96916e87b431ed051dd91a9fc23e62404bf8
ioedeveloper <daviddisu8@gmail.com>
2018-01-22 16:40:15 +0100

    Api Routing

---
+++ api/api/agent.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 1/22/18
+ * Time: 5:35 AM
+ */
+
+class agent
+{
+    public function index($formdata = ''){
+        if($formdata != ''){
+            echo $formdata;
+        }else{
+            echo "Welcome to the new routing";
+        }
+    }
+}
---
+++ api/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by ioedeveloper
+ * Date: 1/22/18
+ * Time: 4:57 AM
+ */
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+require_once "init.php";
+$app = new App();
