266f6d0848c7860d3d530d3a88cbf06136d80084
blackhatjohnny <ogwurujohnson@gmail.com>
2018-01-30 16:49:41 +0100

    working files

---
+++ admin/assets/js/pages/blog/blog.js
@@ -0,0 +1,152 @@
+﻿$(function () {
+    "use strict";  
+    initSparkline();
+    initDonutChart();
+    getMorris('line', 'line_chart');
+});
+
+
+function getMorris(type, element) {
+    
+    if (type === 'line') {
+        Morris.Line({
+            element: element,
+            data: [{
+                'period': '2017 Q3',
+                'Technology': 1021,
+                'Lifestyle': 660,
+                'Sports': 1021
+            }, {
+                    'period': '2017 Q3',
+                    'Technology': 2310,
+                    'Lifestyle': 1050,
+                    'Sports': 951
+                }, {
+                    'period': '2016 Q1',
+                    'Technology': 960,
+                    'Lifestyle': 1260,
+                    'Sports': 947
+                }, {
+                    'period': '2015 Q4',
+                    'Technology': 900,
+                    'Lifestyle': 2010,
+                    'Sports': 935
+                }, {
+                    'period': '2014 Q5',
+                    'Technology': 890,
+                    'Lifestyle': 920,
+                    'Sports': 911
+                },{
+                    'period': '2013 Q2',
+                    'Technology': 918,
+                    'Lifestyle': 600,
+                    'Sports': 813
+                }],
+            xkey: 'period',
+            ykeys: ['Technology', 'Lifestyle', 'Sports'],
+            labels: ['Technology', 'Lifestyle', 'Sports'],
+            lineColors: ['#FFC107', '#ba3bd0', '#CDDC39'],
+            lineWidth: 3
+        });
+    }
+}
+//===============================================================================
+function initSparkline() {
+    $(".sparkline").each(function () {
+        var $this = $(this);
+        $this.sparkline('html', $this.data());
+    });
+}
+//===============================================================================
+function initDonutChart() {
+    Morris.Donut({
+        element: 'donut_chart',
+        data: [{
+            label: 'Chrome',
+            value: 30
+        }, {
+            label: 'Firefox',
+            value: 25
+        }, {
+            label: 'Safari',
+            value: 20
+        }, {
+            label: 'Opera',
+            value: 10        
+        }, {
+            label: 'IE',
+            value: 10
+        },{
+            label: 'Other',
+            value: 5
+        }],
+        colors: ['#00adef', '#fcb711', '#12a682', '#f58787', '#708090', '#ec3b57 '],
+        formatter: function (y) {
+            return y + '%'
+        }
+    });
+}
+//===============================================================================
+var data = [], totalPoints = 110;
+function getRandomData() {
+    if (data.length > 0) data = data.slice(1);
+
+    while (data.length < totalPoints) {
+        var prev = data.length > 0 ? data[data.length - 1] : 50, y = prev + Math.random() * 10 - 5;
+        if (y < 0) { y = 0; } else if (y > 100) { y = 100; }
+
+        data.push(y);
+    }
+
+    var res = [];
+    for (var i = 0; i < data.length; ++i) {
+        res.push([i, data[i]]);
+    }
+
+    return res;
+}
+
+//===============================================================================
+$(window).scroll(function() {
+    $('.card .sparkline').each(function(){
+    var imagePos = $(this).offset().top;
+
+    var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+400) {
+            $(this).addClass("pullUp");
+        }
+    });
+});
+//===============================================================================
+$(".dial1").knob();
+$({animatedVal: 0}).animate({animatedVal: 66}, {
+    duration: 3000,
+    easing: "swing", 
+    step: function() { 
+        $(".dial1").val(Math.ceil(this.animatedVal)).trigger("change"); 
+    }
+});
+$(".dial2").knob();
+$({animatedVal: 0}).animate({animatedVal: 26}, {
+    duration: 3800,
+    easing: "swing", 
+    step: function() { 
+        $(".dial2").val(Math.ceil(this.animatedVal)).trigger("change"); 
+    }
+});
+$(".dial3").knob();
+$({animatedVal: 0}).animate({animatedVal: 76}, {
+    duration: 3200,
+    easing: "swing", 
+    step: function() { 
+        $(".dial3").val(Math.ceil(this.animatedVal)).trigger("change"); 
+    }
+});
+$(".dial4").knob();
+$({animatedVal: 0}).animate({animatedVal: 88}, {
+    duration: 3500,
+    easing: "swing", 
+    step: function() { 
+        $(".dial4").val(Math.ceil(this.animatedVal)).trigger("change"); 
+    }
+});
---
+++ admin/assets/js/pages/ecommerce.js
@@ -0,0 +1,120 @@
+﻿//[custom Javascript]
+
+//Project:	Nexa - Responsive Bootstrap 4 Template
+//Version:  1.0
+//Last change:  15/12/2017
+//Primary use:	Nexa - Responsive Bootstrap 4 Template
+
+//should be included in all pages. It controls some layout
+
+
+$(function () {
+    "use strict";  
+    initSparkline();    
+    getMorris('area', 'area_chart');
+});
+
+
+function getMorris(type, element) {
+    if (type === 'area') {
+        Morris.Area({
+            element: element,
+            data: [{
+                period: '2017 Q5',
+                Sales: 3480,
+                Revenue: 2102,
+                Profit: 2365
+            }, {
+                    period: '2013 Q3',
+                    Sales: 4215,
+                    Revenue: 4460,
+                    Profit: 2028                
+                },{
+                    period: '2014 Q1',
+                    Sales: 4215,
+                    Revenue: 4460,
+                    Profit: 2028
+                }, {
+                    period: '2015 Q4',
+                    Sales: 6412,
+                    Revenue: 5713,
+                    Profit: 3450
+                },{
+                    period: '2016 Q3',
+                    Sales: 4215,
+                    Revenue: 4460,
+                    Profit: 2028                
+                },{
+                    period: '2017 Q5',
+                    Sales: 4215,
+                    Revenue: 4460,
+                    Profit: 2028                
+                }],
+            xkey: 'period',
+            ykeys: ['Sales', 'Revenue', 'Profit'],
+            labels: ['Sales', 'Revenue', 'Profit'],
+            pointSize: 3,
+            hideHover: 'auto',
+            lineColors: ['rgba(254, 191, 15,0.3)', 'rgba(25, 161, 183, 0.3)', 'rgba(134, 139, 239, 0.3)']
+        });
+    }
+}
+//===============================================================================
+function initSparkline() {
+    $(".sparkline").each(function () {
+        var $this = $(this);
+        $this.sparkline('html', $this.data());
+    });
+}
+//===============================================================================
+$(window).scroll(function() {
+    $('.card .sparkline').each(function(){
+    var imagePos = $(this).offset().top;
+
+    var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+400) {
+            $(this).addClass("pullUp");
+        }
+    });
+});
+//===============================================================================
+$(function () {
+    $('#world-map-markers').vectorMap({
+        map: 'world_mill_en',
+        normalizeFunction: 'polynomial',
+        hoverOpacity: 0.7,
+        hoverColor: false,
+        backgroundColor: 'transparent',
+        regionStyle: {
+            initial: {
+                fill: 'rgba(210, 214, 222, 1)',
+                "fill-opacity": 1,
+                stroke: 'none',
+                "stroke-width": 0,
+                "stroke-opacity": 1
+            },
+            hover: {
+                fill: 'rgba(255, 193, 7, 2)',
+                cursor: 'pointer'
+            },
+            selected: {
+                fill: 'yellow'
+            },
+            selectedHover: {}
+        },
+        markerStyle: {
+            initial: {
+                fill: '#fff',
+                stroke: '#FFC107 '
+            }
+        },
+        markers: [
+            { latLng: [37.09,-95.71], name: 'America' },
+            { latLng: [51.16,10.45], name: 'Germany' },
+            { latLng: [-25.27, 133.77], name: 'Australia' },
+            { latLng: [56.13,-106.34], name: 'Canada' },
+            { latLng: [20.59,78.96], name: 'India' },
+            { latLng: [55.37,-3.43], name: 'United Kingdom' },
+        ]
+    });
+});
