58533298e050d62e81c075f30e78406d9d07c733
blackhatjohnny <ogwurujohnson@gmail.com>
2018-02-14 20:57:50 +0100

    all properties

--- user/assets/js/pages/tables/jquery-datatable.js
+++ user/assets/js/pages/tables/jquery-datatable.js
@@ -3,9 +3,15 @@ $(function () {
 
     //Exportable table
     $('.js-exportable').DataTable({
+
         dom: 'Bfrtip',
         buttons: [
             'copy', 'csv', 'excel', 'pdf', 'print'
-        ]
+        ],
+        "ajax":{
+            url :"https://localhost/gafista/api/client/clientproperty", // json datasource
+            type: "post",  // method  , by default get
+
+        }
     });
 });
