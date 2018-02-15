e9e5d5797c95142e75828c2be1ff0b3bdae758e2
tariak2 <tariak2@gmail.com>
2018-02-15 03:39:05 +0100

    wallet and invoice
    wallet and invoice

---
+++ index.php
@@ -0,0 +1,504 @@
+﻿<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+
+	<!-- Meta Tags -->	
+	<meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+	<meta name="description" content="Seobiz – Marketing & Seo Business Template">
+	<meta name="keywords" content="corporate, marketing, optimization, seo, seo business, seo company, seo services, seo website, Social Marketing">
+	<meta name="author" content="Theme Path">
+	<!-- Title -->
+	<title>Gafista Concepts</title>		
+	<!-- Favicon Icon -->
+	<link rel="icon" type="image/png" href="img/favicon.png">	
+	<!-- Apple Touch Icons -->
+	<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="57x57" href="img/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="img/apple-touch-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="img/apple-touch-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="img/apple-touch-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="img/apple-touch-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon-180x180.png">
+	
+	<!-- Stylesheets Start -->
+	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,600,700" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet"> 
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/magnific-popup.css">
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/animate.css">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="css/meanmenu.min.css">
+	<link rel="stylesheet" href="css/responsive.css">	
+	
+</head>
+<body>
+	<!-- Preloader Start -->
+	<div id="preloader">
+		<div id="preloader-status"></div>
+	</div>
+	<!-- Preloader End -->
+	<!-- Header Start -->
+	<header>
+		<div class="header-top">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-9 col-sm-8">
+						<div class="header-left">
+							<ul>
+								<li><i class="fa fa-envelope-o"></i>info@gafistaconcepts.com</li>
+								<li><i class="fa fa-map-marker"></i>35B, Bode Thomas Street, Suru-Lere, Lagos, Nigeria</li>
+							</ul>	
+						</div>
+					</div>				
+					<div class="col-md-3 col-sm-4">
+						<div class="header-tp-right">
+							<div class="header-right dropdown">					
+								<a class="dropdown-toggle" data-toggle="dropdown" href="#">en<span class="fa fa-angle-down"></span></a>	
+								<ul class="language dropdown-menu">
+								  <li><a href="#">English</a></li>
+								  <li><a href="#">Spanish</a></li>
+								  <li><a href="#">Portuguese</a></li>
+								  <li><a href="#">Russian</a></li>
+								  <li><a href="#">Japanese</a></li>
+								  <li><a href="#">Punjabi</a></li>
+								  <li><a href="#">Vietnamese</a></li>
+								</ul>								
+							</div>
+						</div>
+					</div>
+					<div class="appointment">
+						<a href="sign-up.html">SignUp</a>
+					</div>					
+				</div>
+			</div>
+		</div>
+		<!-- Header Start -->
+		<div class="hd-sec">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+						<div class="logo">
+							<a href="index.php"><img src="img/logo.png" alt=""/></a>
+						</div>
+					</div>	
+					<div class="col-md-9">
+						<div class="menu">
+							<nav id="main-menu" class="main-menu">
+								<ul>
+									<li class="active"><a href="index.php">home</a></li>
+															
+									
+										
+                                    
+									<li><a href="#contact">Contact Us</a></li>
+									<li><a href="sign-in.html">Sign-in</a></li>
+								</ul>
+							</nav>
+							<div class="menu-icon">	
+								<div class="site-search">	
+									<i class="fa fa-search"></i>
+									<div class="search-forum">
+										<form action="#">
+											<input placeholder="Search" type="text">
+											<input type="submit" value="Go"/>
+										</form>							
+									</div>							
+								</div>
+							</div>
+						</div>					
+					</div>					
+				</div>
+			</div>
+		</div>
+		<!-- Header End -->
+	</header>
+	<!-- Header End -->	
+	
+	<!-- Slider Section Start -->
+	<div class="slider">
+		<div class="all-slide owl-item">					
+			<div class="single-slide" style="background-image:url(img/slide2.jpg);">
+				<div class="slider-overlay"></div>
+				<div class="slider-text">
+					<div class="slider-caption">
+						<span class="subtitle">Your Best </span>
+						<h1>Waste Management <span>App</span></h1>					
+					</div>	
+					<ul>
+						<li><a href="#about">learn more</a></li>					
+						<li><a href="#contact">contact us </a></li>
+					</ul>
+				</div>
+			</div>		
+			<div class="single-slide" style="background-image:url(img/slide1.jpg);">
+				<div class="slider-overlay"></div>
+				<div class="slider-text">
+					<div class="slider-caption">
+						<span class="subtitle">Get waste out</span>
+						<h1> within a short period of time</h1>					
+					</div>	
+					<ul>
+						<li><a href="sign-up.html">Lets Recycle</a></li>					
+						<li><a href="#contact">contact us </a></li>
+					</ul>
+				</div>
+			</div>				
+			<div class="single-slide" style="background-image:url(img/slide3.jpg);">
+				<div class="slider-overlay"></div>
+				<div class="slider-text">
+					<div class="slider-caption">
+						<span class="subtitle">Do you have</span>
+						<h1>recyclable beverage</h1>					
+					</div>	
+					<ul>
+						<li><a href="sign-up.html">SignUp</a></li>					
+						<li><a href="#contact">contact us </a></li>
+					</ul>
+				</div>
+			</div>																						
+		</div>
+	</div>
+	<!-- Slider Section End -->	
+	<!-- Why Choose Us Start -->
+	<div class="why-choose pt-100 pb-100">
+		<div class="container">			
+			
+			<div class="row">
+				<div class="about-img">
+					<div class="col-md-5">
+						<div class="about-thumb">
+							<img src="img/about.png" alt=""/>
+						</div>					
+					</div>					
+					<div class="col-md-7" id="about">
+						<div class="about-desc">
+							<h1>We provied best solution for your home and offices and Waste Product.</h1>
+							<p>Cleaning up the environment. Thanks to all our supporters around the planet, there is a big difference in the amount of recyclable beverage containers that aren’t making it back to the land fill or into the gutters and out to the ocean where they harm animals and the environment. </p>
+							<div class="about-countup">
+								<div class="counting-inner">
+									<div class="countup-text">
+										<h2 class="counter">5874</h2>
+										<h4>project done</h4>						
+									</div>
+								</div>						
+								<div class="counting-inner">
+									<div class="countup-text">
+										<h2 class="counter">6894</h2>
+										<h4>happy client</h4>						
+									</div>
+								</div>							
+								<div class="counting-inner">
+									<div class="countup-text">
+										<h2 class="counter">218</h2>
+										<h4>total award</h4>						
+									</div>
+								</div>
+							</div>
+							<p> Pick up from your home or office, we’d be happy to coordinate a pick up at your location..</p>
+						</div>				
+					</div>				
+				</div>				
+			</div>			
+		</div>		
+	</div>
+	
+	
+	
+	<!--<div class="pricing-sec pt-100 pb-100">
+		<div class="container">	
+			<div class="row">
+				<div class="col-md-12">
+					<div class="sec-title">
+						<h1>choose your price plan</h1>
+						<div class="border-shape"></div>
+					</div>
+				</div>
+			</div>			
+			<div class="row">
+				<div class="col-md-12">
+				   <ul class="pricing-tab">
+						<li class=""><a data-toggle="tab" href="#Standard" aria-expanded="false">Standard</a></li>
+						<li class="active"><a data-toggle="tab" href="#Smart" aria-expanded="false">Smart</a></li>
+						<li class=""><a data-toggle="tab" href="#Business" aria-expanded="false">Business</a></li>
+					</ul>						
+				</div>
+			</div>	
+			<div class="tab-content">	
+				<div id="Standard" class="tab-pane">				
+					<div class="row">
+						<div class="col-md-6">
+							<img src="img/pricing_pack_2.jpg" alt=""/>
+						</div>				
+						<div class="col-md-6">
+							<div class="pricing-content">
+								<div class="pricing-text">
+									<h1>Each type of hosting service we provide is further optimized with unique performance tweaks</h1>
+									<div class="intro-text">
+										<i class="fa fa-check"></i>
+										<p>8 gbDisck Space</p>
+									</div>							
+									<div class="intro-text">
+										<i class="fa fa-check"></i>
+										<p>10 GB Monthly Bandwidth</p>
+									</div>					
+									<div class="intro-text">
+										<i class="fa fa-check"></i>
+										<p>3 Domains</p>
+									</div>							
+									<div class="intro-text">
+										<i class="fa fa-check"></i>
+										<p>Unlimited FTP Account</p>
+									</div>
+									<p>Lorem ipsum dolor sit amet, lorem nibh lectus urna arcu, lorem erat semper, auctor suspendisse quisque molestie ut. </p>
+									<div class="pricing-price">
+										<span class="amount">59$</span>
+										<span class="pricing-link"><a href="#">get started</a></span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>				
+				<div id="Smart" class="tab-pane active">				
+					<div class="row">
+						<div class="col-md-6">
+							<img src="img/pricing_pack.jpg" alt=""/>
+						</div>				
+						<div class="col-md-6">
+							<div class="pricing-content">
+								<div class="pricing-text">
+									<h1>Each type of hosting service we provide is further optimized with unique performance tweaks</h1>
+									<div class="intro-text">
+										<i class="fa fa-check"></i>
+										<p>10 gbDisck Space</p>
+									</div>							
+									<div class="intro-text">
+										<i class="fa fa-check"></i>
+										<p>21 GB Monthly Bandwidth</p>
+									</div>					
+									<div class="intro-text">
+										<i class="fa fa-check"></i>
+										<p>4 Domains</p>
+									</div>							
+									<div class="intro-text">
+										<i class="fa fa-check"></i>
+										<p>Unlimited FTP Account</p>
+									</div>
+									<p>Lorem ipsum dolor sit amet, lorem nibh lectus urna arcu, lorem erat semper, auctor suspendisse quisque molestie ut. </p>
+									<div class="pricing-price">
+										<span class="amount">29$</span>
+										<span class="pricing-link"><a href="#">get started</a></span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>				
+				<div id="Business" class="tab-pane">				
+					<div class="row">
+						<div class="col-md-6">
+							<img src="img/pricing_pack_3.jpg" alt=""/>
+						</div>				
+						<div class="col-md-6">
+							<div class="pricing-content">
+								<div class="pricing-text">
+									<h1>Each type of hosting service we provide is further optimized with unique performance tweaks</h1>
+									<div class="intro-text">
+										<i class="fa fa-check"></i>
+										<p>15 gbDisck Space</p>
+									</div>							
+									<div class="intro-text">
+										<i class="fa fa-check"></i>
+										<p>25 GB Monthly Bandwidth</p>
+									</div>					
+									<div class="intro-text">
+										<i class="fa fa-check"></i>
+										<p>6 Domains</p>
+									</div>							
+									<div class="intro-text">
+										<i class="fa fa-check"></i>
+										<p>Unlimited FTP Account</p>
+									</div>
+									<p>Lorem ipsum dolor sit amet, lorem nibh lectus urna arcu, lorem erat semper, auctor suspendisse quisque molestie ut. </p>
+									<div class="pricing-price">
+										<span class="amount">69$</span>
+										<span class="pricing-link"><a href="#">get started</a></span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>							
+			</div>				
+		</div>		
+	</div> -->
+	
+	<div class="call-to-action-sec">
+		<div class="call-to-action-overlay"></div>
+		<div class="container">
+			<div class="row">	
+				<div class="col-md-12" id="contact">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.303000983282!2d3.3751324144853045!3d6.483257825429224!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8c7fa29a7f41%3A0xa28207e55d876358!2sThomas+St%2C+Ebute+Metta%2C+Lagos!5e0!3m2!1sen!2sng!4v1518645106351" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>		
+				
+
+						<div class="contact-field">
+						<br>
+					<center>	<h1>Send Us a Message</h1> 	</center>
+						<div class="col-md-6 col-sm-6 col-xs-12">
+							<div class="single-input-field">
+								<input placeholder="First Name" type="text">
+							</div>
+						</div>							
+						<div class="col-md-6 col-sm-6 col-xs-12">
+							<div class="single-input-field">
+								<input placeholder="Last Name" type="text">
+							</div>
+						</div>							
+						<div class="col-md-6 col-sm-6 col-xs-12">
+							<div class="single-input-field">
+								<input placeholder="Your E-mail Address" type="email">
+							</div>
+						</div>							
+						<div class="col-md-6 col-sm-6 col-xs-12">
+							<div class="single-input-field">
+								<input placeholder="phone/mobile number" type="text">
+							</div>
+						</div>											
+						<div class="col-md-12 message-input">
+							<div class="single-input-field">
+								<textarea placeholder="write here your advice or complains"></textarea>
+							</div>
+						</div>																				
+						<div class="single-input-fieldsbtn">
+						<center><div class="submit-form">
+								<div class="call-to-action-text btn">
+									<input value="send message" type="submit"> 	
+								</div>
+								
+							</div></center>
+				
+							</div>
+							</div>
+				</div>												
+			</div>					
+		</div>
+	</div>
+	<!-- Call To Action Section Start -->		
+	<!-- Footer Section Start -->
+	<footer class="footer">	
+		<!-- Footer Top Section Start -->
+		<div class="footer-sec">
+			<div class="container">
+				<div class="row">				
+					<div class="col-md-3 col-sm-6">
+						<div class="footer-wedget-one">
+							<a href="index.php"><img src="img/logo.png" alt=""/></a>
+							<p>Cleaning up the environment. Thanks to all our supporters around the planet.</p>
+							<form action="#">
+								<input placeholder="your email address" type="email">
+								<button type="submit"><i class="fa fa-paper-plane"></i></button>	
+							</form>							
+						</div>
+					</div>											
+					<div class="col-md-3 col-sm-6">
+						<div class="footer-widget-menu">
+							<h2>our services</h2>
+							<ul>
+								<li><a href="#">marketing</a></li>
+								<li><a href="#">optimization</a></li>
+								<li><a href="#">support</a></li>
+								<li><a href="#">investment</a></li>
+								<li><a href="#">marketing  strateecies</a></li>
+							</ul>
+						</div>
+					</div>	
+					<div class="col-md-3 col-sm-6">
+						<div class="footer-wedget-two">
+							<h2>letest tag </h2>
+							<ul>
+								<li><a href="#">content</a></li>
+								<li><a href="#">cost</a></li>
+								<li><a href="#">optimiz</a></li>
+								<li><a href="#">online</a></li>
+								<li><a href="#">sales</a></li>
+								<li><a href="#">expert</a></li>								
+							</ul>
+						</div>
+					</div>					
+					<div class="col-md-3 col-sm-6">
+						<div class="footer-wedget-four">
+							<h2>contact us </h2>
+								<div class="inner-box">
+									<div class="media">
+										<div class="inner-item">
+											<div class="media-left">
+												<span class="icon"><i class="fa fa-map-marker"></i></span>
+											</div>				
+											<div class="media-body">
+												<span class="inner-text">35B, Bode Thomas Street, Suru-Lere, Lagos.</span>
+											</div>											
+										</div>					
+									</div>								
+									<div class="media">
+										<div class="inner-item">
+											<div class="media-left">
+												<span class="icon"><i class="fa fa-envelope-o"></i></span>
+											</div>				
+											<div class="media-body">
+												<span class="inner-text">info@gafistaconcepts.com</span>
+											</div>											
+										</div>					
+									</div>											
+								</div>
+								<ul>
+									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-google"></i></a></li>
+									<li><a href="#"><i class="fa fa-skype"></i></a></li>
+								</ul>							
+						</div>
+					</div>				
+				</div>
+			</div>
+		</div>
+		<!-- Footer Top Section End -->
+		<!-- Footer Bottom Section Start -->
+		<div class="footer-bottom-sec">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="copy-right">
+								<center><a href="index.php"><img src="img/logo.png" alt=""/></a></center>
+							<p>Copyright 2018 &copy; <span><a href="#">Gafista Concepts Limited</a></p>
+						</div>
+					</div>					
+				</div>
+			</div>
+		</div>
+		<!-- Footer Bottom Section End -->
+	</footer>
+	<!-- Footer Section End -->
+	<!-- Scripts Js Start -->
+    <script src="js/jquery-2.2.4.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/isotope.pkgd.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/owl.animate.js"></script>
+	<script src="js/jquery.scrollUp.min.js"></script>
+	<script src="js/jquery.counterup.min.js"></script>
+	<script src="js/modernizr.min.js"></script>
+	<script src="js/waypoints.min.js"></script>
+	<script src="js/jquery.meanmenu.min.js"></script>
+	<script src="js/custom.js"></script>
+	<!-- Scripts Js End -->	
+</body>
+
+<!-- Mirrored from themeearth.com/tf/html/seobiz/ by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 20 Jan 2018 00:04:44 GMT -->
+</html>
