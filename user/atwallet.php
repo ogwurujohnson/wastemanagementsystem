04bfa13e0e2a1afeebe90046b867b8ef3f52bd09
blackhatjohnny <ogwurujohnson@gmail.com>
2018-05-22 12:58:46 +0100

    update to UI

--- user/atwallet.php
+++ user/atwallet.php
@@ -52,46 +52,7 @@
         <div class="navbar-header"> <a href="javascript:void(0);" class="h-bars"></a> <a class="navbar-brand" href="index.html">Gafista</a></div>
         <ul class="nav navbar-nav navbar-right">
             <li><a href="javascript:void(0);" class="js-search" data-close="true"><i class="zmdi zmdi-search"></i></a></li>
-            <li class="dropdown"> <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button"><i class="zmdi zmdi-notifications"></i>
-                <div class="notify"><span class="heartbit"></span><span class="point"></span></div>
-                </a>
-                <ul class="dropdown-menu slideDown">
-                    <li class="header">NOTIFICATIONS</li>
-                    <li class="body">
-                        <ul class="menu list-unstyled">
-                            <li> <a href="javascript:void(0);">
-                                <div class="icon-circle l-coral"> <i class="material-icons">person_add</i> </div>
-                                <div class="menu-info">
-                                    <h4>12 new members joined</h4>
-                                    <p> <i class="material-icons">access_time</i> 14 mins ago </p>
-                                </div>
-                                </a> </li>
-                            <li> <a href="javascript:void(0);">
-                                <div class="icon-circle l-turquoise"> <i class="material-icons">add_shopping_cart</i> </div>
-                                <div class="menu-info">
-                                    <h4>4 sales made</h4>
-                                    <p> <i class="material-icons">access_time</i> 22 mins ago </p>
-                                </div>
-                                </a> </li>                            
-                            <li> <a href="javascript:void(0);">
-                                <div class="icon-circle l-seagreen"> <i class="material-icons">cached</i> </div>
-                                <div class="menu-info">
-                                    <h4><b>John</b> updated status</h4>
-                                    <p> <i class="material-icons">access_time</i> 3 hours ago </p>
-                                </div>
-                                </a> </li>
-                            <li> <a href="javascript:void(0);">
-                                <div class="icon-circle l-blue"> <i class="material-icons">settings</i> </div>
-                                <div class="menu-info">
-                                    <h4>Settings updated</h4>
-                                    <p> <i class="material-icons">access_time</i> Yesterday </p>
-                                </div>
-                                </a> </li>
-                        </ul>
-                    </li>
-                    <li class="footer"> <a href="javascript:void(0);">View All Notifications</a> </li>
-                </ul>
-            </li>
+            
             
             <li><a href="sign-in.html" class="mega-menu" data-close="true"><i class="zmdi zmdi-power"></i></a></li>
             
@@ -123,7 +84,7 @@
                                     
                 </ul>
             </li>
-            <li><a href="userprofile.html">Verify Payments Made in Bank</a>
+            <li><a href="verifypay.php">Verify Payments Made in Bank</a>
 
             <li><a href="javascript:void(0)">Property</a>
                 <ul class="pullDown">
@@ -182,11 +143,11 @@
                                     <input type="text" id="email" class="form-control" name="useremail"  required disabled>
                                 </div>
                                 <div class="form-line">
-                                    <input type="text" id="firstname" class="form-control" name="firstname" required>
+                                    <input type="text" id="firstname" class="form-control" name="firstname" placeholder="Payers Firstname" required>
                                     
                                 </div>
                                 <div class="form-line">
-                                    <input type="text" id="lastname" class="form-control" name="lastname" required>
+                                    <input type="text" id="lastname" class="form-control" name="lastname" placeholder="Payers Lastname" required>
                                     
                                 </div>
                                 <div class="form-line">
